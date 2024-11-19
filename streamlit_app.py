f3fcb05437c6aacdd13e80e221451ab6dc5d4752
223207406 <raulvidaurrazaga@gmail.com>
2024-11-19 15:09:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("PutitaS ricotas")
+st.title("proyecto bioinformatica de los mas chilos faze xavy good crazyyyyyy MxAlexRA08")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
